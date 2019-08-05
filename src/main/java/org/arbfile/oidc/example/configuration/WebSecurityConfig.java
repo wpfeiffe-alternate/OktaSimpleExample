aeb3b00ce8586d83352efb6bfff25f0aa5673a99
jkennedy <jkennedy@arbfile.org>
2019-08-05 17:53:32 -0400

    Changes made after code was committed. Example is complete now.

--- src/main/java/org/arbfile/oidc/example/configuration/WebSecurityConfig.java
+++ src/main/java/org/arbfile/oidc/example/configuration/WebSecurityConfig.java
@@ -1,13 +1,8 @@
 package org.arbfile.oidc.example.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter
@@ -21,7 +16,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
             .oauth2Login()
             .and()
             .oauth2Client();
-
         //http.csrf().disable();
     }
 
@@ -34,19 +28,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
 			.formLogin().and()
 			.httpBasic();
      */
-
-    @Autowired
-    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.inMemoryAuthentication()
-            .withUser("user")
-            .password(passwordEncoder().encode("Password1")).roles("USER");
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    {
-        return new BCryptPasswordEncoder();
-    }
 
 }
--- src/main/java/org/arbfile/oidc/example/controller/IndexController.java
+++ src/main/java/org/arbfile/oidc/example/controller/IndexController.java
@@ -2,9 +2,12 @@ package org.arbfile.oidc.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -14,8 +17,11 @@ public class IndexController
 {
     private Logger logger = LoggerFactory.getLogger(IndexController.class);
 
+    @Autowired
+    private Environment environment;
+
     @RequestMapping({"/", "/index"})
-    public String index(Principal principal)
+    public String index(Principal principal, Model model)
     {
         if (principal == null)
         {
@@ -31,6 +37,27 @@ public class IndexController
                 logger.info("username = " + user.getAttributes().get("preferred_username"));
             }
         }
+        model.addAttribute("profile", this.environment.getActiveProfiles()[0]);
         return "index";
+    }
+
+    @RequestMapping({"/welcome"})
+    public String welcome(Principal principal, Model model)
+    {
+        if (principal == null)
+        {
+            logger.info("Principal was not found");
+            model.addAttribute("name", "");
+        }
+        else
+        {
+            if ( principal instanceof OAuth2AuthenticationToken)
+            {
+                OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) principal;
+                OAuth2User user = authentication.getPrincipal();
+                model.addAttribute("name", user.getAttributes().get("preferred_username"));
+            }
+        }
+        return "welcome";
     }
 }
