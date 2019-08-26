8e20501b530c0b69e9b6de2458beb6a1dc94ebe9
jkennedy <jkennedy@arbfile.org>
2019-08-26 17:48:13 -0400

    This commit contains code (really an attempt) to add an AuthenticationProvider.

--- server/src/main/java/org/arbfile/oidc/example/oauth/CustomOidcUserService.java
+++ server/src/main/java/org/arbfile/oidc/example/oauth/CustomOidcUserService.java
@@ -2,22 +2,33 @@ package org.arbfile.oidc.example.oauth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Component;
 
-public class CustomOidcUserService extends OidcUserService
+@Component
+public class CustomOidcUserService implements UserDetailsService
 {
     private Logger logger = LoggerFactory.getLogger(CustomOidcUserService.class);
 
+
     @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        logger.info("running AF user service with " + username);
+        return null;
+    }
+
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException
     {
-        OidcUser user = super.loadUser(userRequest);
-        logger.info("running AF user service");
-        CustomOidcUser afUser = new CustomOidcUser((DefaultOidcUser)user);
-        return user;
+        //OidcUser user = super.loadUser(userRequest);
+//        logger.info("running AF user service");
+//        CustomOidcUser afUser = new CustomOidcUser((DefaultOidcUser)user);
+//        return user;
+        return null;
     }
 }
